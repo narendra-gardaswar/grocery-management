6d1a54cfc14c7fc978a77d5a9eacf5a7c982bf1d
Narendra <narendragardaswar@gmail.com>
2024-11-18 01:38:15 +0530

    feat: added all exception filter

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,11 +3,21 @@ import { ConfigModule } from '@nestjs/config';
 import { configOptions } from './configs/config';
 import { AuthModule } from '@auth/auth.module';
 import { DrizzleProvider, PostgresProvider } from './database';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './filters/all-exception-filter';
 
 @Global()
 @Module({
   imports: [ConfigModule.forRoot(configOptions), AuthModule],
-  providers: [Logger, PostgresProvider, DrizzleProvider],
+  providers: [
+    Logger,
+    PostgresProvider,
+    DrizzleProvider,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
   exports: [PostgresProvider, DrizzleProvider],
 })
 export class CoreModule {}
